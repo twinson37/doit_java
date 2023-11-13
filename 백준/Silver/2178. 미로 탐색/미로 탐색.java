9dfc25b8f4da330e74efe07a0c1e0df4cbe95604
twinson37 <62787090+twinson37@users.noreply.github.com>
2023-11-14 02:19:59 +0900

    [Silver I] Title: 미로 탐색, Time: 256 ms, Memory: 19592 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -3,59 +3,12 @@ import java.util.*;
 
 public class Main {
 
+    static int n;
+    static int m;
     static int[][] maze;
     static boolean[][] visited;
-    static int n, m;
-    static Block[][] blocks;
-    static int minimum = Integer.MAX_VALUE;
-
-    static class Block {
-        int i, j;
-        boolean north;
-        boolean south;
-        boolean east;
-        boolean west;
-
-        Block(int i, int j) {
-            this.i = i;
-            this.j = j;
-
-            checkPath();
-        }
-
-        public boolean isNorth() {
-            return north;
-        }
-
-        public boolean isSouth() {
-            return south;
-        }
-
-        public boolean isEast() {
-            return east;
-        }
-
-        public boolean isWest() {
-            return west;
-        }
-
-        private void checkPath() {
-
-            if (i > 0 && maze[i - 1][j] == 1) {
-                this.north = true;
-            }
-            if (i < n - 1 && maze[i + 1][j] == 1) {
-                this.south = true;
-            }
-            if (j > 0 && maze[i][j - 1] == 1) {
-                this.west = true;
-            }
-            if (j < m - 1 && maze[i][j + 1] == 1) {
-                this.east = true;
-            }
-
-        }
-    }
+    static int[] dx = {0,1,0,-1};
+    static int[] dy = {1,0,-1,0};
 
     public static void main(String[] args) {
 
@@ -66,8 +19,6 @@ public class Main {
         maze = new int[n][m];
         visited = new boolean[n][m];
 
-        blocks = new Block[n][m];
-
         for (int i = 0; i < n; i++) {
 
             String row = sc.next();
@@ -77,63 +28,36 @@ public class Main {
                 maze[i][j] = Integer.parseInt(String.valueOf(row.charAt(j)));
             }
         }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                blocks[i][j] = new Block(i, j);
-            }
-        }
-
-        findWayOut(1, 0, 0);
-
-        System.out.println(minimum);
-
+        findWayOut(0, 0);
+        System.out.println(maze[n-1][m-1]);
     }
 
-    private static void findWayOut(int depth, int y, int x) {
+    private static void findWayOut(int y, int x) {
 
-        Queue<Block> queue = new LinkedList<>();
+        Queue<int[]> queue = new LinkedList<>();
         int[][] arr = new int[n][m];
 
-        queue.add(blocks[y][x]);
-        visited[y][x] = true;
+        queue.add(new int[]{ 0,0 });
+
         arr[0][0] = 1;
+        visited[0][0] = true;
         while (!queue.isEmpty()){
 
-            Block nowBlock = queue.poll();
-            int nowY = nowBlock.i;
-            int nowX = nowBlock.j;
+            int[] nowCoordinate = queue.poll();
+            int nowY = nowCoordinate[0];
+            int nowX = nowCoordinate[1];
 
-            if (nowY == n - 1 && nowX == m - 1) {
-                minimum = Math.min(minimum,arr[nowY][nowX]);
-                return ;
-            }
+            for(int i =0;i<4;i++){
+                int nextX = nowX+dx[i];
+                int nextY = nowY+dy[i];
 
-            if ((nowBlock.isEast() && !visited[nowY][nowX + 1])) {
-
-                visited[nowY][nowX + 1] = true;
-                queue.add(blocks[nowY][nowX+1]);
-                arr[nowY][nowX+1] = arr[nowY][nowX]+1;
-            }
-
-            if (nowBlock.isNorth() && !visited[nowY - 1][nowX]) {
-
-                visited[nowY - 1][nowX] = true;
-                queue.add(blocks[nowY-1][nowX]);
-                arr[nowY-1][nowX] = arr[nowY][nowX]+1;
-            }
-            if (nowBlock.isWest() && !visited[nowY][nowX - 1]) {
-
-                visited[nowY][nowX - 1] = true;
-                queue.add(blocks[nowY][nowX-1]);
-                arr[nowY][nowX-1] = arr[nowY][nowX]+1;
-
-            }
-            if (nowBlock.isSouth() && !visited[nowY + 1][nowX]) {
-
-                visited[nowY + 1][nowX] = true;
-                queue.add(blocks[nowY+1][nowX]);
-                arr[nowY+1][nowX] = arr[nowY][nowX]+1;
+                if(nextX>=0&&nextY>=0&&nextY<n&&nextX<m){
+                    if (maze[nextY][nextX]!=0&&!visited[nextY][nextX]){
+                        visited[nextY][nextX] = true;
+                        maze[nextY][nextX] = maze[nowY][nowX]+1;
+                        queue.add(new int[]{nextY,nextX});
+                    }
+                }
             }
 
         }
